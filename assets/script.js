f1dbf0fc4b944be0f89d148d328c500d85132f10
infinotiver <88645086+infinotiver@users.noreply.github.com>
2025-07-11 20:13:02 +0530

    enhance

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,19 @@
 // script.js
 
-window.addEventListener('scroll', () => {
-    const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 100) {
-      navbar.classList.add('scrolled');
-    } else {
-      navbar.classList.remove('scrolled');
+
+async function updateBuildInfo() {
+    try {
+        const response = await fetch('https://api.github.com/repos/infinotiver/infinotiver.github.io/commits');
+        const commits = await response.json();
+
+        const lastCommit = commits[0]?.sha.substring(0, 7);
+        const lastUpdate = commits[0]?.commit.author.date
+
+        document.getElementById('last-commit').textContent = lastCommit || 'N/A';
+        document.getElementById('last-update').textContent = lastUpdate || 'N/A';
+    } catch (error) {
+        console.error('Error fetching GitHub data:', error);
     }
-  });
-  
+}
+
+updateBuildInfo();
