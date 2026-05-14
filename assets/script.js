20ea113a02cca8df50ce68ebc80530c94a4dccc4
infinotiver <88645086+infinotiver@users.noreply.github.com>
2026-05-14 21:59:26 +0530

    chore: packages cleanup

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,3 @@
-// script.js
-
-
 async function updateBuildInfo() {
     try {
         const response = await fetch('https://api.github.com/repos/infinotiver/infinotiver.github.io/commits');
@@ -9,8 +6,19 @@ async function updateBuildInfo() {
         const lastCommit = commits[0]?.sha.substring(0, 7);
         const lastUpdate = commits[0]?.commit.author.date
 
+        const localDate = new Date(lastUpdate).toLocaleString('en-US', {
+            year: 'numeric',
+            month: 'short',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+            timeZoneName: 'short'
+
+        });
+
+        console.log(localDate)
         document.getElementById('last-commit').textContent = lastCommit || 'N/A';
-        document.getElementById('last-update').textContent = lastUpdate || 'N/A';
+        document.getElementById('last-update').textContent = localDate || 'N/A';
     } catch (error) {
         console.error('Error fetching GitHub data:', error);
     }
